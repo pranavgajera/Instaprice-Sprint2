e90fc8a6b58a59040ffbaacaa9388e028cd3f436
ete2njit <ec2-user@ip-172-31-54-167.ec2.internal>
2020-11-05 23:47:25 +0000

    Change Content.jsx from object oriented to functional

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -1,14 +1,13 @@
 import * as React from 'react';
 import GoogleButton from './GoogleButton';
 
-export class Content extends React.Component {
-    render() {
-        return(
-            <div>
-                <h1>
-                    Hello World from React!
-                </h1>
-                <GoogleButton />
-            </div>);
-    }
+export function Content() {
+    return(
+        <div>
+            <h1>
+                Hello World from React!
+            </h1>
+            <GoogleButton />
+        </div>);
+
 }
