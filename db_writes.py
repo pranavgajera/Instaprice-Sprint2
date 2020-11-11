33ea924ad1ec9432a5cea3c0eab48c130c9183cf
sz376 <ec2-user@ip-172-31-40-69.ec2.internal>
2020-11-11 21:05:25 +0000

    removed unneeded lines of code

--- db_writes.py
+++ db_writes.py
@@ -2,12 +2,8 @@
 Handles the database writes for app
 """
 import os
-import pickle
-import requests
 import dotenv
 import psycopg2
-from api_calls import mock_price_history, mock_search_response
-
 
 DOTENV_PATH = os.path.join(os.path.dirname(__file__), "secret_tokens.env")
 dotenv.load_dotenv(DOTENV_PATH)
@@ -17,27 +13,6 @@ SQL_PWD = os.environ["SQL_PASSWORD"]
 DBUSER = os.environ["USER"]
 DATABASE_URI = os.environ["DATABASE_URL"]
 con = psycopg2.connect(database='postgres', user=SQL_USER, password=SQL_PWD)
-
-def emit_all_items(channel):
-    all_itemnames = [db_itemname.itemname for db_itemname in db.session.query(models.Posts).all()]
-    all_imageurls = [db_imageurl.imageurl for db_imageurl in db.session.query(models.Posts).all()]
-    all_pricehists = [db_pricehist.pricehist for db_pricehist in db.session.query(models.Posts).all()]
-    all_usernames = [db_username.username for db_username in db.session.query(models.Posts).all()]
-    all_pfps = [db_pfp.pfp for db_pfp in db.session.query(models.Posts).all()]
-    all_times = [db_time.time for db_time in db.session.query(models.Posts).all()]
-    
-    socketio.emit(
-        channel,
-        {
-            "allItemnames": all_itemnames,
-            "allImageurls": all_imageurls,
-            "allPricehists": all_pricehists,
-            "allUsernames": all_usernames,
-            "allPfps": all_pfps,
-            "allTimes": all_times,
-        },
-    )
-  
 
 def price_write(price_data):
     with con:
