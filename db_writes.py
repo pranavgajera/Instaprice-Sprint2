a62d7dac3adad317d0dbd219288f689aa1accbfd
ete2njit <ec2-user@ip-172-31-54-167.ec2.internal>
2020-11-13 21:23:20 +0000

    lint python code

--- db_writes.py
+++ db_writes.py
@@ -12,11 +12,12 @@ SQL_DB = os.environ["SQL_DB"]
 DBUSER = os.environ["USER"]
 DATABASE_URI = os.environ["DATABASE_URL"]
 
-con = psycopg2.connect(database=SQL_DB, user=SQL_USER, password=SQL_PWD, host="localhost")
+CON = psycopg2.connect(database=SQL_DB, user=SQL_USER, password=SQL_PWD, host="localhost")
 
 def price_write(price_data):
-    with con:
-        cur = con.cursor()
+    """write data to the database"""
+    with CON:
+        cur = CON.cursor()
         price_list = price_data['priceHistory']
         price_list_str = ''
         for entry in price_list:
@@ -26,18 +27,20 @@ def price_write(price_data):
         poster = price_data['user']
         pfp = "temp profile picture"
         time = price_data['time']
-        cur.execute("INSERT INTO posts (itemname, imageurl, pricehist, username, pfp, time) VALUES (%s, %s, %s, %s, %s, %s);", (item, imageurl, price_list_str, poster, pfp, time))
-        
+        cur.execute("INSERT INTO posts (itemname, imageurl, pricehist, username, pfp, time) " + \
+            "VALUES (%s, %s, %s, %s, %s, %s);", (item, imageurl, price_list_str, poster, pfp, time))
+
 def get_posts(username):
-    with con:
-        cur = con.cursor()
+    """get posts from a specific user from the database"""
+    with CON:
+        cur = CON.cursor()
         cur.execute(f"SELECT * FROM posts WHERE username = '{username}'")
         rows = cur.fetchall()
         item_data = {
-            'itemname': rows[0][1], 
-            'imgurl': rows[0][2], 
-            'pricehistory': rows[0][3], 
-            'user': rows[0][4], 
+            'itemname': rows[0][1],
+            'imgurl': rows[0][2],
+            'pricehistory': rows[0][3],
+            'user': rows[0][4],
             'pfp': rows[0][5],
             'time': rows[0][6]
             }
