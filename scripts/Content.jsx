25250795b4186947d76a54e05d64213f46bb8c00
ete2njit <ec2-user@ip-172-31-54-167.ec2.internal>
2020-11-13 16:30:45 +0000

    eslint react files

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -6,62 +6,59 @@ import Socket from './Socket';
 import LiveFeed from './LiveFeed';
 import Feed from './Feed';
 
-import "./Content.css";
+import './Content.css';
 
 export default function Content() {
-    const [authenticated, setAuthentication] = useState(false);
-    const [searched, setSearched] = React.useState(false);          // true if we need to display a search list
-    const [searchList, setSearchList] = React.useState([]);
-    
-    function getSearchList() {
-        React.useEffect(() => {
-            Socket.on('search response', (data) => {
-                setSearchList(data['search_list']);
-                console.log("got list: ", data);
-                setSearched(true);
-            });
-        }, []);
-    }
-    
-    getSearchList();
+  const [authenticated, setAuthentication] = useState(false);
+  const [searched, setSearched] = React.useState(false); // true if we need to display a search list
+  const [searchList, setSearchList] = React.useState([]);
 
-    useEffect(() => {
+  function getSearchList() {
+    React.useEffect(() => {
+      Socket.on('search response', (data) => {
+        setSearchList(data.search_list);
+        //console.log('got list: ', data);
+        setSearched(true);
+      });
+    }, []);
+  }
+
+  getSearchList();
+
+  useEffect(() => {
     Socket.on('connected', (data) => {
       setAuthentication(true);
     });
+  }, []);
 
-    }, []);
+  if (!authenticated) {
+    return (
+      <div className="LoginPage">
+        <h1>
+          <img src="./static/instapricelogo.png" alt="InstaPrice" />
+        </h1>
+        <GoogleButton />
+      </div>
+    );
+  }
 
-    
-    if(!authenticated) {
-        return (
-            <div className="LoginPage">
-                <h1>
-                    <img src={ "./static/instapricelogo.png" } alt = 'InstaPrice' />
-                </h1>
-                <GoogleButton />
-            </div>
-        );
-    }
+  return (
+    <div className="HomePage">
+      <h1>
+        <img src="./static/instapricelogo.png" alt="InstaPrice" />
+        <LiveFeed />
+      </h1>
+      { searched
+        ? (
+          <SearchResults searchList={searchList} />
+        ) : (null)}
 
-    return(
-        <div className="HomePage">
-            <h1>
-                <img src={ "./static/instapricelogo.png" } alt = 'InstaPrice' />
-                <LiveFeed />
-            </h1>
-            { searched ? 
-            (
-                <SearchResults searchList={ searchList } />
-            ) : (null)
-            }
-
-            <div className="searchbar">
-              <SearchBar setSearched={ setSearched }/>
-            </div>
-            <div className="Feed">
-            <Feed />
-            </div>
-        </div>);
-
+      <div className="searchbar">
+        <SearchBar setSearched={setSearched} />
+      </div>
+      <div className="Feed">
+        <Feed />
+      </div>
+    </div>
+  );
 }
