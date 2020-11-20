14de7d4ca2472c4ff036653c3e545715091e9f20
domo106 <jasonsanimations@gmail.com>
2020-11-20 17:06:51 -0500

    Added saving graph to file, and added main method.
    So the generate_graph function will now take a post_id as input, which it'll use to assign a unique filename to the graph, which can also be stored to the database.
    
    Also added a main method so the file doesn't run code to test the functionality when importing.
    
    Also went with horizontal gridlines.

--- graph_functions.py
+++ graph_functions.py
@@ -7,28 +7,23 @@ import matplotlib.dates as mdates
 import matplotlib
 import numpy as np
 
-def generate_graph(entry_list, multiplier=1.0):
+def generate_graph(entry_list, post_id="test"):
 	"""Takes two inputs:
 	entry_list - the output of the price_history function, a list of dicts
-	multiplier - A float from 0.25 to 1.0, the fraction of dates that go on the graph.
-	1.0 is an entire year, 0.25 is three months, 1/12 is one month.
-	Takes most recent dates.
+	post_id - the unique id representing the post in the DB. Used to assign filename.
 	Returns nothing at the moment. Should return the image_url for the generated graph.
 	"""
 	date_list = []
 	price_list = []
-	num_points = int(len(entry_list)*multiplier)
+	num_points = int(len(entry_list))
 	first_point = len(entry_list)-num_points
 	last_point = len(entry_list)
 	for i in range(first_point, last_point):
 		entry = entry_list[i]
 		date_string = entry["price_date"]
 		entry_datetime = datetime.datetime.strptime(date_string, '%m/%d/%Y')
-		print(type(entry_datetime))
 		date_list.append(entry_datetime)
 		price_list.append(entry["price"])
-
-
 
 	# data
 	df=pd.DataFrame({'x': date_list, 'y': price_list })
@@ -40,7 +35,6 @@ def generate_graph(entry_list, multiplier=1.0):
 	plt.title("Price History")
 
 	# Add horizontal lines
-	plt.gca().xaxis.grid()
 	plt.gca().yaxis.grid()
 
 	# Format x axis to have dates
@@ -53,32 +47,30 @@ def generate_graph(entry_list, multiplier=1.0):
 	plt.gca().yaxis.set_major_formatter(formatter)
 
 	# Display in window
-	plt.show()
+	# plt.show()
+	file_name = "graphs/graph_{}.png".format(post_id)
+	plt.savefig(file_name)
 
 def loadPickle(filename):
 	"""Local helper function to open a pickled response"""
 	file = open(filename, 'rb')
 	return pickle.load(file)
 
-#search_results = amazon_search_test.search_products("Camera")
-#savePickle("search_results",search_results)
+def main():
+	"""Tests the generate_graph function"""
+	search_results = loadPickle("search_results.pkl")
+	ASIN = search_results.json()[0]["ASIN"]
+	print(ASIN)
 
-search_results = loadPickle("search_results.pkl")
-ASIN = search_results.json()[0]["ASIN"]
-print(ASIN)
+	entry_list = price_history_test.mock_price_history("B07X6C9RMF")
 
-entry_list = price_history_test.mock_price_history("B07X6C9RMF")
-#savePickle("price_history.pkl", price_history)
-
-#price_history = loadPickle("price_history.pkl")
-#price_list = price_history.json()['price_history']
-print(entry_list[:10])
-single_entry = entry_list[0]
-single_date = single_entry['price_date']
-print(single_date)
-single_datetime = pd.to_datetime([single_date], format='%m/%d/%Y')
-print(single_datetime)
+	print(entry_list[:10])
+	single_entry = entry_list[0]
+	single_date = single_entry['price_date']
+	single_datetime = pd.to_datetime([single_date], format='%m/%d/%Y')
 
 
-divisor = 1.0
-generate_graph(entry_list, divisor)
+	generate_graph(entry_list, "Test ID")
+
+if __name__ == '__main__':
+    main()
