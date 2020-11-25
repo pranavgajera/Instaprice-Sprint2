26377c813c9682397022b165373afa9f93f835be
Pranav Gajera <pranavgajera@gmail.com>
2020-11-25 12:29:50 -0500

    changed some styling

--- scripts/PriceHistoryResults.jsx
+++ scripts/PriceHistoryResults.jsx
@@ -44,8 +44,7 @@ export default function PriceHistoryResults({ ASIN }) {
                   {pricehistory.map((item) => (
                     <li>
                       {item.price_date}
-                      -$
-                      {item.price}
+                      -${item.price}
                     </li>
                   ))}
                 </ul>)
