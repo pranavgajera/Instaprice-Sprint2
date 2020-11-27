9ef16296d77a7a763bce02d9b113ae8036f21373
Jeffrey Kuo <ec2-user@ip-172-31-5-49.ec2.internal>
2020-11-27 19:53:26 +0000

    fixed content not updated with link

--- scripts/Content.jsx
+++ scripts/Content.jsx
@@ -4,8 +4,11 @@ import SearchResults from './SearchResults';
 import SearchBar from './SearchBar';
 import Socket from './Socket';
 import Feed from './Feed';
-import FacebookButton from "./FacebookButton"
+import ProfilePage from './ProfilePage';
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import FacebookButton from "./FacebookButton";
 import '../style/Content.css';
+
 
 export default function Content() {
   const [authenticated, setAuthentication] = useState(false);
@@ -13,19 +16,33 @@ export default function Content() {
   const [searchList, setSearchList] = React.useState([]);
   const [username, setUsername] = useState('');
   const [profpic, setProfpic] = useState('');
+  const [profileOf, setProfileOf] = useState("");
 
   function getSearchList() {
     React.useEffect(() => {
       Socket.on('search response', (data) => {
-        setSearchList(data.search_list);
-        // console.log('got list: ', data);
+        if(Array.isArray(data.search_list)) {
+          setSearchList(data.search_list);
+        } else {
+          setSearchList([]);
+        }
         setSearched(true);
       });
     }, []);
   }
   
+  function getProfilePage() {
+    React.useEffect(() => {
+      Socket.on('make profile page', (data) => {
+        setProfileOf(data.username);
+        console.log("This is the page for: /" + data.username);
+      });
+    }, []);
+  }
+  
+  getProfilePage();
   getSearchList();
-
+  
   useEffect(() => {
     Socket.on('connected', (data) => {
       setAuthentication(true);
@@ -47,28 +64,43 @@ export default function Content() {
   }
 
   return (
-    <div className="HomePage">
-      { searched
-        ? (
-          <SearchResults
-            searchList={searchList}
-            username={username}
-            pfp={profpic}
-            closeSearchList={() => setSearched(false)}
-          />
-        ) : (null)}
-        
-      <div className="Content">
-        <h1>
-          <img src="./static/instapricelogo.png" alt="InstaPrice" />
-        </h1>
-        <div className="searchbar">
-          <SearchBar />
-        </div>
-        <div className="Feed">
-          <Feed />
-        </div>
-      </div>
+    <div>
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/'>
+            <div className="HomePage">
+              { searched
+                ? (
+                  <SearchResults
+                    searchList={searchList}
+                    username={username}
+                    pfp={profpic}
+                    closeSearchList={() => setSearched(false)}
+                  />
+                ) : (null)}
+                
+              <div className="Content">
+                <h1>
+                  <img src="./static/instapricelogo.png" alt="InstaPrice" />
+                </h1>
+                <div className="searchbar">
+                  <SearchBar />
+                </div>
+                <div className="Feed">
+                  <Feed />
+                </div>
+              </div>
+          </div>
+          </Route>
+          <Route path={'/' + profileOf}>
+            <ProfilePage
+              username={profileOf}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
+    
+    
   );
 }
